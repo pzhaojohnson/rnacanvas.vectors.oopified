908de4d792f3e753bef4353b1f0591dbae00fcc3
Philip Johnson <p.zhao.johnson@gmail.com>
2024-11-18 19:25:12 -0500

    Add magnitude and direction setters to Vector class

--- src/Vector.test.js
+++ src/Vector.test.js
@@ -36,6 +36,18 @@ describe('Vector class', () => {
     expect(vector.magnitude).toBeCloseTo(64.47480127925948);
   });
 
+  test('`set magnitude()`', () => {
+    let vector = new Vector(29, -15);
+
+    vector.magnitude = 120;
+    expect(vector.magnitude).toBeCloseTo(120);
+
+    expect(vector.direction).toBeCloseTo(-0.4773453823736719);
+
+    expect(vector.x).toBeCloseTo(120 * Math.cos(-0.4773453823736719));
+    expect(vector.y).toBeCloseTo(120 * Math.sin(-0.4773453823736719));
+  });
+
   test('`direction` getter', () => {
     let vector = new Vector(100, 0);
     expect(vector.direction).toBeCloseTo(0);
@@ -49,5 +61,17 @@ describe('Vector class', () => {
     vector.x = -74;
     vector.y = -128;
     expect(vector.direction).toBeCloseTo(-2.09497595557781);
+  });
+
+  test('`set direction()`', () => {
+    let vector = new Vector(57, 88);
+
+    vector.direction = (-1.2) * Math.PI;
+    expect(vector.direction - ((-1.2) * Math.PI) % (2 * Math.PI)).toBeCloseTo(2 * Math.PI);
+
+    expect(vector.magnitude).toBeCloseTo((57**2 + 88**2)**0.5);
+
+    expect(vector.x).toBeCloseTo((57**2 + 88**2)**0.5 * Math.cos((-1.2) * Math.PI));
+    expect(vector.y).toBeCloseTo((57**2 + 88**2)**0.5 * Math.sin((-1.2) * Math.PI));
   });
 });
--- src/Vector.ts
+++ src/Vector.ts
@@ -21,11 +21,25 @@ export class Vector {
     return Math.sqrt(this.x**2 + this.y**2);
   }
 
+  set magnitude(magnitude) {
+    let direction = this.direction;
+
+    this.x = magnitude * Math.cos(direction);
+    this.y = magnitude * Math.sin(direction);
+  }
+
   /**
    * The angle that is the direction of the vector
    * in the standard Cartesian coordinate system.
    */
   get direction(): number {
     return Math.atan2(this.y, this.x);
+  }
+
+  set direction(direction) {
+    let magnitude = this.magnitude;
+
+    this.x = magnitude * Math.cos(direction);
+    this.y = magnitude * Math.sin(direction);
   }
 }
